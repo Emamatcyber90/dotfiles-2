6550566f546382d0f805f96f52bec905441208b7
DictumMortuum <theio.vrefos@gmail.com>
2017-07-25 11:23:15 +0300

    slimming down

--- .zsh/autoload/alias.zsh
+++ .zsh/autoload/alias.zsh
@@ -1,19 +1,7 @@
 require alias && return 1
 
-alias apt-update="sudo apt-get --yes update ; sudo apt-get --yes upgrade ; sudo apt-get --yes autoclean ; sudo apt-get --yes autoremove"
-
-# https://news.ycombinator.com/item?id=11070797
-# http://stackoverflow.com/questions/1030169/easy-way-pull-latest-of-all-submodules
-alias dot="git --git-dir=$DOT_HOME/dotfiles.git --work-tree=$DOT_HOME"
-
 # Assumes HIST_IGNORE_SPACE is set
 # making aliases to certain commands to start with space to be ignored from history
 alias ls=" ls --color=tty"
 alias ll=" ls -lh"
 alias history=" fc -il 1"
-
-# Common typos
-# Warning: highly personalized
-alias ψδ=" cd"
-alias λσ=" ls"
-alias γιτ="git"
--- .zsh/autoload/plugins.zsh
+++ .zsh/autoload/plugins.zsh
@@ -1,18 +1,7 @@
-# Plugins
-[[ $ZSH_VERSION == <5->* ]] && source $PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-source $PLUGINS/zsh-async/async.plugin.zsh
-source $PLUGINS/pure/pure.plugin.zsh
-
-export BASE16_THEME=base16-ashes
-. $DOT_HOME/.base16_theme
-
-_base16() {
-  local script=$1
-  local theme=$2
-  [ -f $script ] && . $script
-  ln -fs $script $DOT_HOME/.base16_theme
-  export BASE16_THEME=${theme}
-  echo -e "if \0041exists('g:colors_name') || g:colors_name != 'base16-$theme'\n  colorscheme base16-$theme\nendif" >| ~/.vimrc_background
-}
-
-alias base16_ashes="_base16 \"$DOT_HOME/.dotfiles/base16-shell/scripts/base16-ashes.sh\" ashes"
+if [[ $ZSH_VERSION == <5->* ]]; then
+  source $PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+  source $PLUGINS/zsh-async/async.plugin.zsh
+  source $PLUGINS/pure/pure.plugin.zsh
+else
+  source $THEMES/simple.zsh-theme
+fi
